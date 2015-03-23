5ac960315322716ddde37d54f28f6383dca5bff8
Alex Wilmer <djazium@gmail.com>
2015-03-22 23:08:22 -0400

    Session variables, equal temperment, octave switch

--- client/notepad.js
+++ client/notepad.js
@@ -1,95 +1,147 @@
+Meteor.startup(function() {
+  Session.set('baseFrequency', 27.5);
+  Session.set('noteHeight', 13);
+  Session.set('octave',  1);
+  Session.set('waveShape', 'square');
+});
+
+Template.notepad.helpers({
+  octave: function() {
+    return Session.get('octave');
+  }
+});
+
+Template.notepad.events({
+  'click .octave i': function (event) {
+    Session.set('octave',
+      event.target.className.indexOf('left') > -1
+      ? Session.get('octave') - 1
+      : Session.get('octave') + 1
+    );
+  }
+});
+
 Template.notepad.rendered = function() {
-  var audioContext = new (window.AudioContext || window.webkitAudioContext)()
-    , oscillator = audioContext.createOscillator()
-    , frequency = 1000
-    , waveShape = 'sine'
-    , gain = audioContext.createGain()
-    , notepad = document.getElementById("touch-layer")
-    , canvasContext = notepad.getContext('2d')
-    , clicking = false
-    , noteHeight = 13
 
-    , drawNotes = function (noteHeight) {
-        for (var i = 0; i < noteHeight; i++) {
-          noteY = notepad.height / noteHeight;
-          canvasContext.beginPath();
-          canvasContext.rect(0, i * noteY, notepad.width, noteY);
-          canvasContext.fillStyle = '#2b2b2b';
-          canvasContext.fill();
-        }
-      }
+var audioContext = new (window.AudioContext || window.webkitAudioContext)()
 
-    , playNote = function(y, height) {
+  // iOS requires user input to trigger audio
+  , audioPlaying = false
+  , clicking = false
 
-        oscillator.frequency.value = (Math.floor(y / height) * 100) + 200;
-        console.log(oscillator.frequency.value);
-        gain.gain.value = 1;
-        canvasContext.beginPath();
-        canvasContext.clearRect(0, y, canvasContext.canvas.clientWidth, height);
-      }
+  // oscillator
+  , oscillator = audioContext.createOscillator()
+  , gain = audioContext.createGain()
 
-    , toggleGrid = function(noteHeight) {
-        for (var i = 1; i < noteHeight; i++) {
-          canvasContext.beginPath();
-          canvasContext.moveTo(0, i * (notepad.height / noteHeight));
-          canvasContext.lineTo(canvasContext.canvas.clientWidth, i * (notepad.height / noteHeight));
-          canvasContext.stroke();
-        }
-      }
+  // canvas
+  , notepad = document.getElementById("touch-layer")
+  , canvasContext = notepad.getContext('2d')
 
-    , mousedown = function (event) {
+  , drawNotes = function (noteHeight) {
+      for (var i = 0; i < noteHeight; i++) {
         noteY = notepad.height / noteHeight;
+        canvasContext.beginPath();
+        canvasContext.rect(0, i * noteY, notepad.width, noteY);
+        canvasContext.fillStyle = '#2b2b2b';
+        canvasContext.fill();
+      }
+    }
+
+  , flipSetPosition = function (n, first, last) {
+      return -n + first + last;
+    }
+
+  , getFrequency = function (baseFrequency, semitones) {
+      return baseFrequency * Math.pow(Math.pow(2, 1/12), semitones);
+    }
+
+  , playNote = function (y, height) {
+      // audio
+      oscillator.frequency.value =
+        getFrequency(Session.get('baseFrequency') * Math.pow(2, Session.get('octave')), flipSetPosition(Math.floor(y / height), 0, 12));
+      console.log(oscillator.frequency.value);
+      gain.gain.value = 1;
+
+      // temporarily remove top layer
+      canvasContext.beginPath();
+      canvasContext.clearRect(0, y, canvasContext.canvas.clientWidth, height);
+    }
+
+  , toggleGrid = function (noteHeight) {
+      for (var i = 1; i < noteHeight; i++) {
+        canvasContext.beginPath();
+        canvasContext.moveTo(0, i * (notepad.height / noteHeight));
+        canvasContext.lineTo(canvasContext.canvas.clientWidth, i * (notepad.height / noteHeight));
+        canvasContext.stroke();
+      }
+    }
+
+  , mousedown = function (event) {
+      noteY = notepad.height / Session.get('noteHeight');
+      clickedNote = Math.floor(event.offsetY / noteY);
+      playNote(noteY * clickedNote, noteY);
+      clicking = true;
+      if (!audioPlaying) {
+        oscillator.start();
+        audioPlaying = true;
+      }
+    }
+
+  , mousemove = function (event) {
+      if (clicking) {
+        noteY = notepad.height / Session.get('noteHeight');
         clickedNote = Math.floor(event.offsetY / noteY);
         playNote(noteY * clickedNote, noteY);
-        clicking = true;
       }
+    }
 
-    , mousemove = function (event) {
-        if (clicking) {
-          noteY = notepad.height / noteHeight;
-          clickedNote = Math.floor(event.offsetY / noteY);
-          playNote(noteY * clickedNote, noteY);
-        }
+  , mouseup = function (event) {
+      clicking = false;
+      gain.gain.value = 0;
+      drawNotes(Session.get('noteHeight'));
+      toggleGrid(Session.get('noteHeight'));
+    }
+
+  , touchstart = function (event) {
+      event.preventDefault();
+      noteY = canvasContext.canvas.clientHeight / Session.get('noteHeight');
+      clickedNote = Math.floor((event.targetTouches[0].pageY - $('#touch-layer').offset().top) / noteY);
+      playNote(noteY * clickedNote, noteY);
+      if (!audioPlaying) {
+        oscillator.start();
+        audioPlaying = true;
       }
+    }
 
-    , mouseup = function (event) {
-        clicking = false;
-        gain.gain.value = 0;
-        drawNotes(noteHeight);
-        toggleGrid(noteHeight);
-      }
+  , touchend = function (event) {
+      event.preventDefault();
+      gain.gain.value = 0;
+      drawNotes(Session.get('noteHeight'));
+      toggleGrid(Session.get('noteHeight'));
+    }
 
-    , touchstart = function (event) {
-        event.preventDefault();
-        noteY = canvasContext.canvas.clientHeight / noteHeight;
-        clickedNote = Math.floor((event.targetTouches[0].pageY - $('#touch-layer').offset().top) / noteY);
-        playNote(noteY * clickedNote, noteY);
-      }
+  , touchcancel = function (event) {
+      event.preventDefault();
+      console.log('touchcancel');
+    }
 
-    , touchend = function (event) {
-        event.preventDefault();
-        gain.gain.value = 0;
-        drawNotes(noteHeight);
-        toggleGrid(noteHeight);
-      }
+  , touchleave = function (event) {
+      event.preventDefault();
+      console.log('touchleave');
+    }
 
-    , touchcancel = function (event) {
-        event.preventDefault();
-        console.log('touchcancel');
-      }
+  , touchmove = function (event) {
+      event.preventDefault();
+      noteY = canvasContext.canvas.clientHeight / Session.get('noteHeight');
+      clickedNote =
+        Math.floor(
+          (event.targetTouches[0].pageY - $('#touch-layer').offset().top) / noteY
+        );
+      playNote(noteY * clickedNote, noteY);
+    };
 
-    , touchleave = function (event) {
-        event.preventDefault();
-        console.log('touchleave');
-      }
 
-    , touchmove = function (event) {
-        event.preventDefault();
-        noteY = canvasContext.canvas.clientHeight / noteHeight;
-        clickedNote = Math.floor((event.targetTouches[0].pageY - $('#touch-layer').offset().top) / noteY);
-        playNote(noteY * clickedNote, noteY);
-      };
-
+  // hookup event handlers
   notepad.addEventListener("mousedown", mousedown, false);
   notepad.addEventListener("mousemove", mousemove, false);
   notepad.addEventListener("mouseup", mouseup, false);
@@ -103,15 +155,14 @@ Template.notepad.rendered = function() {
   notepad.width = canvasContext.canvas.clientWidth;
   notepad.height = canvasContext.canvas.clientHeight;
 
-  drawNotes(noteHeight);
-  toggleGrid(noteHeight);
+  drawNotes(Session.get('noteHeight'));
+  toggleGrid(Session.get('noteHeight'));
   $('#reveal-layer').show();
 
   // initialize audio
   gain.gain.value = 0;
   oscillator.connect(gain);
   gain.connect(audioContext.destination);
-  oscillator.type = waveShape;
-  oscillator.frequency.value = frequency; // value in hertz
-  oscillator.start();
+  oscillator.type = Session.get('waveShape');
+  oscillator.frequency.value = Session.get('baseFrequency'); // value in hertz
 }

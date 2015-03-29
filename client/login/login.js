ea23d18ab8c4ee23ad25d30169975ded721fe02a
Alex Wilmer <djazium@gmail.com>
2015-03-29 03:46:59 -0400

    login with github working all around

--- client/login/login.js
+++ client/login/login.js
@@ -1,3 +1,11 @@
+Template.login.helpers({
+  userName: function () {
+    if (Meteor.user()) {
+      return Meteor.user().profile.name;
+    }
+  }
+});
+
 Template.login.events({
   'click .fa-github': function () {
     Meteor.loginWithGithub({
@@ -7,6 +15,23 @@ Template.login.events({
       if (err) {
         Session.set('errorMessage', err.reason || 'Unknown error');
       }
+
+      else {
+        $('.modal').closeModal();
+      }
     });
+  }
+
+, 'click .modal-trigger': function (event) {
+    $(event.target.hash).openModal();
+  }
+
+, 'click .sign-out': function () {
+    Meteor.logout();
+    $('.modal').closeModal();
+  }
+
+, 'click .cancel': function () {
+    $('.modal').closeModal();
   }
 });
